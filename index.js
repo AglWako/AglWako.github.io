18c14609697c058420e615ee46e0e0b747c0eea7
AglWako <124411918+AglWako@users.noreply.github.com>
2023-02-04 12:00:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("幹");
+console.log("儒林走了就沒了是不是");
+
+
+
+//This is a comment
+
+
+
+
+    function sum(){
+        var a = parseFloat(document.getElementById('a').value);        
+        var b = parseFloat(document.getElementById('b').value);
+        document.getElementById('c').value= a+b
+    }
+    function minus(a,b){
+        var a = parseFloat(document.getElementById('a').value);        
+        var a = parseFloat(document.getElementById('b').value);
+        document.getElementById('c').value = a-b
+    }
+    function multiple(a,b){
+        var a = parseFloat(document.getElementById('a').value);        
+        var b = parseFloat(document.getElementById('b').value);
+        document.getElementById('c').value = a*b
+    }
+    function devide(a,b){
+        var a = parseFloat(document.getElementById('a').value);        
+        var a = parseFloat(document.getElementById('b').value);        
+        document.getElementById('c').value = a/b
+    }
+    console.log('c');
